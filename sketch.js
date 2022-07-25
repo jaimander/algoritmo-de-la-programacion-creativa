442f930924d97e595c88aaf189c1a83c091b4612
Jaime Rodriguez Gómez <jaimerodriguezgo@gmail.com>
2022-07-25 16:51:27 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,9 +9,12 @@ let practiceTime = 0;
 let creativeCodingOutput = 0;
 let coding = false;
 let serendipity = 0;
+let frustrationTolerance = 1.0;
+let frustrationTolerance10 = 10;
 
 let fontRegular, fontItalic, fontSemiBold, fontMono;
 let textStartY = 170;
+let textStartY2 = 190;
 let barStartY = 157;
 let barHeight = 15;
 let language = 'ES';
@@ -77,53 +80,54 @@ function draw() {
     text('coding', 220, textStartY + 120);
     text('practiceTime', 220, textStartY + 150);
     text('serendipity', 220, textStartY + 180);
-    text('creativity', 220, textStartY + 210);
-    text('creativeCodingOutput', 220, textStartY + 280);
+    text('frustrationTolerance', 220, textStartY + 210);
+    text('creativity', 220, textStartY + 240);
+    text('creativeCodingOutput', 220, textStartY + 310);
 
     textAlign(LEFT);
     textSize(15);
     textFont(fontSemiBold);
-    text('controls', 50, textStartY + 340);
+    text('controls', 50, textStartY2 + 340);
     textFont(fontRegular);
     text(
       '[K] and [W]: to increase and decrease playfulAttitude.',
       50,
-      textStartY + 360
+      textStartY2 + 360
     );
-    text('[P]: to active o deactive coding state.', 50, textStartY + 380);
+    text('[P]: to active o deactive coding state.', 50, textStartY2 + 380);
 
     textFont(fontSemiBold);
-    text('how it works?', 500, textStartY + 340);
+    text('description', 500, textStartY2 + 340);
     textFont(fontRegular);
     text(
       'i. The time you spend programming will be reflected in the results.',
       500,
-      textStartY + 360
+      textStartY2 + 360
     );
     text(
       'ii. If you have a playful attitude, it is very likely that you will enter a state of flow.',
       500,
-      textStartY + 380
+      textStartY2 + 380
     );
     text(
       'iii. If you are in a state of flow, your attention and curiosity will grow.',
       500,
-      textStartY + 400
+      textStartY2 + 400
     );
     text(
       'iv. If you are programming and you enter a state of flow, it is likely that you will have a moment of serendipity..',
       500,
-      textStartY + 420
+      textStartY2 + 420
     );
     text(
       'v. Your creativity will be fed by your spirit of play, your level of attention, the curiosity you have, and the experience you have gained with practice.',
       500,
-      textStartY + 440
+      textStartY2 + 440
     );
     text(
       'vi. The results you achieve will depend on your creativity, practice time and whether inspiration finds you programming.',
       500,
-      textStartY + 460
+      textStartY2 + 460
     );
   }
 
@@ -132,60 +136,62 @@ function draw() {
     text('estadoDeFlujo', 220, textStartY + 30);
     text('atención', 220, textStartY + 60);
     text('curiosidad', 220, textStartY + 90);
-    text('programando', 220, textStartY + 120);
+    text('programar', 220, textStartY + 120);
     text('tiempoDePráctica', 220, textStartY + 150);
-    text('serendípia', 220, textStartY + 180);
-    text('creatividad', 220, textStartY + 210);
+    text('serendipia', 220, textStartY + 180);
+    text('toleranciaFrustración', 220, textStartY + 210);
+    text('creatividad', 220, textStartY + 240);
+    text('programaciónCreativa', 220, textStartY + 310);
     //textFont(fontSemiBold);
-    text('programaciónCreativa', 220, textStartY + 280);
+
 
     textAlign(LEFT);
     textSize(15);
     textFont(fontSemiBold);
-    text('controles', 50, textStartY + 340);
+    text('controles', 50, textStartY2 + 340);
     textFont(fontRegular);
     text(
       '[K] and [W]: aumentar o reducir la actitud de juego.',
       50,
-      textStartY + 360
+      textStartY2 + 360
     );
     text(
       '[P]: activar o desactivar estado de la acción de programar.',
       50,
-      textStartY + 380
+      textStartY2 + 380
     );
     textFont(fontSemiBold);
-    text('funcionamiento', 500, textStartY + 340);
+    text('descripción', 500, textStartY2 + 340);
     textFont(fontRegular);
     text(
       'i. El tiempo que dediques a programar se verá reflejado en los resultados.',
       500,
-      textStartY + 360
+      textStartY2 + 360
     );
     text(
       'ii. Si tienes una actitud de juego es muy probable que entres en estado de flujo.',
       500,
-      textStartY + 380
+      textStartY2 + 380
     );
     text(
       'iii. Si estás en estado de flujo, tu atención y tu curiosidad crecerán.',
       500,
-      textStartY + 400
+      textStartY2 + 400
     );
     text(
       'iv. Si estás programando y entras en estado de flujo es probable que tengas un momento de serendípia.',
       500,
-      textStartY + 420
+      textStartY2 + 420
     );
     text(
       'v. Tu creatividad se alimentará de tu espiritu de juego, tu nivel de atención, la curiosidad que tengas, y la experiencia que hayas ganado con la práctica.',
       500,
-      textStartY + 440
+      textStartY2 + 440
     );
     text(
       'vi. Los resultados que logres dependerán de tu creatividad, el tiempo de práctica y de que la inspiración te encuentre programando.',
       500,
-      textStartY + 460
+      textStartY2 + 460
     );
   }
 
@@ -202,8 +208,11 @@ function draw() {
   text(coding, 250, textStartY + 120);
   text(practiceTime, 250, textStartY + 150);
   text(serendipity, 250, textStartY + 180);
-  text(creativity, 250, textStartY + 210);
-  text(creativeCodingOutput, 250, textStartY + 280);
+  frustrationTolerance = frustrationTolerance10*0.1;
+  //text(frustrationTolerance, 250, textStartY + 210);
+  text(nf(frustrationTolerance, 0, 1), 250, textStartY + 210);
+  text(creativity, 250, textStartY + 240);
+  text(creativeCodingOutput, 250, textStartY + 310);
 
   // creativeCoding
   if (frameCount % 20 == 0) {
@@ -239,12 +248,18 @@ function draw() {
   rect(barX, barStartY + 150, practiceTime * 0.1, barHeight);
   // serendipity
   rect(barX, barStartY + 180, serendipity * 10, barHeight);
+  
+  // frustration Tolerance
+  rect(barX, barStartY + 210, frustrationTolerance * 100, barHeight);
+
   // creativity
-  rect(barX, barStartY + 210, creativity * 0.1, barHeight);
+  rect(barX, barStartY + 240, creativity * 0.1, barHeight);
+   
+  
   // creativeCodingOutput
   //fill(255, 100, 0);
   fill(190, 255, 0);
-  rect(barX, barStartY + 280, creativeCodingOutput * 0.01, barHeight);
+  rect(barX, barStartY + 310, creativeCodingOutput * 0.01, barHeight);
 
   //image(pg, 0, 0);
 
@@ -287,7 +302,8 @@ function creativeCoding() {
     attention = 0;
   }
 
-  creativeCodingOutput = int(creativity * 0.1) * practiceTime * int(coding);
+  creativeCodingOutput = int(creativity * 0.1) * practiceTime * int(coding) * frustrationTolerance;
+  creativeCodingOutput = int(creativeCodingOutput);
   return creativeCodingOutput;
 }
 
@@ -315,6 +331,24 @@ function keyPressed() {
       coding = true;
     }
   }
+
+  if (key == 't' || key == 'T') {
+    if (frustrationTolerance10< 10) {
+    frustrationTolerance10 += 1;
+  }
+  }
+  if (key == 'r' || key == 'R') {
+    if (frustrationTolerance10> 0) {
+      frustrationTolerance10 -= 1;  
+    }
+  }
+/*
+  if (key == 't' || key == 'T' || key == 'r' || key == 'R') {
+    frustrationTolerance*0.1;
+  }
+  */
+  
+
 }
 
 function preload() {
